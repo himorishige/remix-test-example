88626f33196474ac857fe46a123b585e821444c7
Hiroshi Morishige <hiroshi.morishige@gmail.com>
2022-09-04 14:46:39 +0900

    add: dummy key

--- app/lib/microcmsClient.server.ts
+++ app/lib/microcmsClient.server.ts
@@ -1,6 +1,6 @@
 import { createClient } from 'microcms-js-sdk';
 
 export const client = createClient({
-  serviceDomain: process.env.MICROCMS_SERVICE_DOMAIN!,
-  apiKey: process.env.MICROCMS_API_KEY!,
+  serviceDomain: process.env.MICROCMS_SERVICE_DOMAIN ?? 'dummyDomain',
+  apiKey: process.env.MICROCMS_API_KEY ?? 'dummyKey',
 });
